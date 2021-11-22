09c6ac6c277eba0f92e3bafb018568485f0deecd
thien vuong <phuongtrucluu12082000@gmail.com>
2021-11-23 03:58:11 +0700

    Add Todo-CategoryViewModel

---
+++ ViewModel/Todo-CategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GCD0806.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GCD0806.ViewModel
+{
+    public class Todo_CategoryViewModel
+    {
+        public Todo Todo { get; set; }
+        public IEnumerator<Category> Category { get; set; }
+
+    }
+}
